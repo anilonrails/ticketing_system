f4333118e6f99b8833500b58b9150763a19097c1
Anıl DEMİR <anilaeus@gmail.com>
2024-02-13 16:04:26 +0300

    We mutated the ticket data on the CreatTicket page

---
+++ app/Filament/Resources/TicketResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TicketResource\Pages;
+use App\Filament\Resources\TicketResource\RelationManagers;
+use App\Models\Ticket;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TicketResource extends Resource
+{
+    protected static ?string $model = Ticket::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->columns(1)
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->autofocus()
+                    ->required()
+                    ->maxLength(255)
+                    ->placeholder('Title'),
+                Forms\Components\Textarea::make('description')->rows(4)->nullable(),
+
+                Forms\Components\Select::make('assigned_to')->relationship('assignedTo', 'name'),
+                Forms\Components\Select::make('status')->options(Ticket::STATUS),
+                Forms\Components\Select::make('priority')->options(Ticket::PRIORITY),
+                Forms\Components\Textarea::make('comment')->rows(4)->nullable(),
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTickets::route('/'),
+            'create' => Pages\CreateTicket::route('/create'),
+            'edit' => Pages\EditTicket::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,10 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'status', 'priority', 'comment', 'assigned_to', 'assigned_by'];
+
+    const PRIORITY = [
+        'Low' => 'Low',
+        'Medium' => 'Medium',
+        'High' => 'High'
+    ];
+    const STATUS = [
+        'Open' => 'Open',
+        'Closed' => 'Closed',
+        'Archived' => 'Archived'
+    ];
+
+    public function assignedTo(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function assignedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_by');
+    }
+
 }
